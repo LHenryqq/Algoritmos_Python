233d8091ad68b760f41babe08e768bb34cb54913
LHenryqq <luis.cristaldo.ads@gmail.com>
2025-03-24 21:53:54 -0400

    Update listas mutáveis.py

--- APRENDENDO_PYTHON/listas mutáveis.py
+++ APRENDENDO_PYTHON/listas mutáveis.py
@@ -35,11 +35,37 @@ lista = ["a", "b","c", "d","e","f"]
 del lista[1:5]
 print(lista)
 
-#adicionar elemento
+#adicionar elemento (.append())
 a = [81, 82, 83]
 a.append(5)
 print (a)
 
+#ordenar elementos (.sort()); (.sort(reverse = True) -> ordem inversa)
 a = [88, 81, 82, 83]
 a.sort()
+print(a)
+
+a = [88, 81, 82, 83]
+a.sort(reverse = True)
+print(a)
+
+#Posição na lista (.index())
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a.index(4))
+
+#adicionar elemento na posição solicitada (.insert(posição, o que desea inserir))
+a = [88, 81, 82, 83]
+a.insert(1, 100)
+print(a)
+
+#contar quantas vezes aparece (.count())
+a = [88, 81, 82, 83, 88, 85, 88, 86]
+print (a.count(88))
+
+#excluir elemento (.pop(posição que deseja excluir)); (.pop()) -> Exclui o último item
+a = [88, 81, 82, 83, 88, 85, 88, 86]
+a.pop()
+print(a)
+
+a.pop(0)
 print(a)
