c408f5995eacc4510e4c5d2e4c14d9336ba15c9b
LHenryqq <luis.cristaldo.ads@gmail.com>
2025-04-09 21:52:26 -0400

    Create exercícios 09-04-25.py

---
+++ APRENDENDO_PYTHON/Exercícios python/exercícios 09-04-25.py
@@ -0,0 +1,39 @@
+#Faça um programa que peça 2 números e imprima o maior deles
+# num_1 = (input("Digite um número: "))
+# num_2 = (input("Digite outro número: "))
+# if(num_1 > num_2):
+#     print("O Primeiro número é maior")
+# else:
+#     print("O Segundo número é maior")
+
+#Informe se o número é positivo, neutro ou negativo
+# num = int(input("Informe um número: "))
+# if (num == 0):
+#     print("Neutro")
+# elif (num > 0):
+#     print("Positivo")
+# else:
+#     print("Negativo")
+
+#Receber notas, exibir média e dizer se passou ou não
+# n1 = float(input("Nota 1: "))
+# n2 = float(input("Nota 2: "))
+# media = (n1+n2)/2
+# print("A média é:", media)
+# if media >= 7:
+#     print("Aprovado")
+# else:
+#     print("Reprovado")
+
+#Calcular reajuste salário de funcionário
+salario = float(input("Digite o valor do salário: R$"))
+if salario < 500:
+    reajuste = salario*(15/100)
+elif salario >= 500 and salario < 1000:
+    reajuste = salario*(10/100)
+else:
+    reajuste = salario*(5/100)
+salario_atual = salario + reajuste
+print("O Salário inicial era: R$", salario)
+print("A correção que deve ser feita é de: R$", reajuste)
+print("O Salário com o reajuste será de: R$", salario_atual)
