3763b9ee1eb205ee68558b16aa7af308ba70e2f0
LHenryqq <luis.cristaldo.ads@gmail.com>
2025-04-30 19:38:44 -0400

    Update while.py

--- APRENDENDO_PYTHON/APRENDENDO WHILE/while.py
+++ APRENDENDO_PYTHON/APRENDENDO WHILE/while.py
@@ -69,36 +69,72 @@
 #         break
 
 #cadastro
+# while True:
+#     while True:
+#         nome = input("Digite seu nome: ").upper()
+#         if len(nome) < 3:
+#             print("Erro no nome. O nome deve ter no mínimo 3 caracteres.")
+#         else:
+#             break
+#     while True:
+#         idade = int(input("Digite sua idade: "))
+#         if idade < 0 or idade > 150:
+#             print("Erro na Idade. Digite uma idade válida.")
+#         else:
+#             break
+#     while True:
+#         salario = float(input("Digite seu salário: R$"))
+#         if salario <= 0:
+#             print("Erro no Salário. Digite um salário válido.")
+#         else:
+#             break
+#     while True:
+#         sexo = input("Digite seu sexo: F(Feminino), M(masculino), O(Outro): ").upper()
+#         if sexo != "F" and sexo != "M" and sexo != "O":
+#             print("Erro. Digite um sexo válido.")
+#         else:
+#             break
+#     while True:
+#         ec = input("Digite seu Estado Civil: S(Solteiro), C(Casado), V(Viúvo(a)), D(Divorciado(a)): ").upper()
+#         if ec != "S" and ec != "C" and ec != "V" and ec != "D":
+#             print("Erro. Digite um Estado Civil Válido.")
+#         else:
+#             print("Cadastro Concluído.")
+#         break
+#     break
+
+#URNA ELETRÔNICA
 while True:
+    cand1 = 0
+    cand2 = 0
+    cand3 = 0
+    cand4 = 0
+    nulo = 0
+    branco = 0
     while True:
-        nome = input("Digite seu nome: ").upper()
-        if len(nome) < 3:
-            print("Erro no nome. O nome deve ter no mínimo 3 caracteres.")
-        else:
+        voto = input("Digite o número que deseja votar: ")
+
+        if voto == "1":
+            cand1 = cand1 + 1
+        elif voto == "2":
+            cand2 = cand2 + 1
+        elif voto == "3":
+            cand3 = cand3 + 1
+        elif voto == "4":
+            cand4 = cand4 + 1
+        elif voto == "5":
+            nulo = nulo + 1
+        elif voto == "6":
+            branco = branco + 1
+        elif voto == "0":
             break
-    while True:
-        idade = int(input("Digite sua idade: "))
-        if idade < 0 or idade > 150:
-            print("Erro na Idade. Digite uma idade válida.")
         else:
-            break
-    while True:
-        salario = float(input("Digite seu salário: R$"))
-        if salario <= 0:
-            print("Erro no Salário. Digite um salário válido.")
-        else:
-            break
-    while True:
-        sexo = input("Digite seu sexo: F(Feminino), M(masculino), O(Outro): ").upper()
-        if sexo != "F" and sexo != "M" and sexo != "O":
-            print("Erro. Digite um sexo válido.")
-        else:
-            break
-    while True:
-        ec = input("Digite seu Estado Civil: S(Solteiro), C(Casado), V(Viúvo(a)), D(Divorciado(a)): ").upper()
-        if ec != "S" and ec != "C" and ec != "V" and ec != "D":
-            print("Erro. Digite um Estado Civil Válido.")
-        else:
-            print("Cadastro Concluído.")
-        break
+            print("Digite um número válido.")
+    print("Resultado: ")
+    print("Votos Candidato 1: ", cand1)
+    print("Votos Candidato 2: ", cand2)
+    print("Votos Candidato 3: ", cand3)
+    print("Votos Candidato 4: ", cand4)
+    print("Votos Nulos: ", nulo)
+    print("Votos em branco: ", branco)
     break
